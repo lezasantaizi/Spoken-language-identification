eab203496d744b4c170a878fac1c12e05c3f537f
lezasantaizi <rxm1989.happy@163.com>
2018-09-25 11:08:11 +0800

    init

---
+++ create_spectrograms.py
@@ -0,0 +1,125 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import scipy.io.wavfile as wav
+from numpy.lib import stride_tricks
+import PIL.Image as Image
+import os
+import argparse
+import glob
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--input_dir", type=str, default='/movie/audio/topcoder/topcoder_train',help="")
+parser.add_argument("--save_img_dir", type=str, default="/movie/audio/topcoder/topcoder_train_png_8k",help="")
+parser.add_argument("--audio_type", type=str, default="mp3",help="audio type")
+
+opt = parser.parse_args()
+print opt
+
+
+""" short time fourier transform of audio signal """
+def stft(sig, frameSize, overlapFac=0.5, window=np.hanning):
+    win = window(frameSize)
+    hopSize = int(frameSize - np.floor(overlapFac * frameSize))
+    
+    # zeros at beginning (thus center of 1st window should be for sample nr. 0)
+    samples = np.append(np.zeros(int(np.floor(frameSize/2.0))), sig)
+    # cols for windowing
+    cols = int(np.ceil( (len(samples) - frameSize) / float(hopSize)) + 1)
+    # zeros at end (thus samples can be fully covered by frames)
+    samples = np.append(samples, np.zeros(frameSize))
+    
+    frames = stride_tricks.as_strided(samples, shape=(cols, frameSize), strides=(samples.strides[0]*hopSize, samples.strides[0])).copy()
+    frames *= win
+    
+    return np.fft.rfft(frames)    
+    
+""" scale frequency axis logarithmically """    
+def logscale_spec(spec, sr=44100, factor=20., alpha=1.0, f0=0.9, fmax=1):
+    spec = spec[:, 0:256]
+    timebins, freqbins = np.shape(spec)
+    scale = np.linspace(0, 1, freqbins) #** factor
+    
+    # http://ieeexplore.ieee.org/xpl/login.jsp?tp=&arnumber=650310&url=http%3A%2F%2Fieeexplore.ieee.org%2Fiel4%2F89%2F14168%2F00650310
+    scale = np.array(map(lambda x: x * alpha if x <= f0 else (fmax-alpha*f0)/(fmax-f0)*(x-f0)+alpha*f0, scale))
+    scale *= (freqbins-1)/max(scale)
+
+    newspec = np.complex128(np.zeros([timebins, freqbins]))
+    allfreqs = np.abs(np.fft.fftfreq(freqbins*2, 1./sr)[:freqbins+1])
+    freqs = [0.0 for i in range(freqbins)]
+    totw = [0.0 for i in range(freqbins)]
+    for i in range(0, freqbins):
+        if (i < 1 or i + 1 >= freqbins):
+            newspec[:, i] += spec[:, i]
+            freqs[i] += allfreqs[i]
+            totw[i] += 1.0
+            continue
+        else:
+            # scale[15] = 17.2
+            w_up = scale[i] - np.floor(scale[i])
+            w_down = 1 - w_up
+            j = int(np.floor(scale[i]))
+           
+            newspec[:, j] += w_down * spec[:, i]
+            freqs[j] += w_down * allfreqs[i]
+            totw[j] += w_down
+            
+            newspec[:, j + 1] += w_up * spec[:, i]
+            freqs[j + 1] += w_up * allfreqs[i]
+            totw[j + 1] += w_up
+    
+    for i in range(len(freqs)):
+        if (totw[i] > 1e-6):
+            freqs[i] /= totw[i]
+    
+    return newspec, freqs
+
+""" plot spectrogram"""
+def plotstft(audiopath, binsize=2**10, plotpath=None, colormap="gray", channel=0, name='tmp.png', alpha=1, offset=0):
+    samplerate, samples = wav.read(audiopath)
+    samples = samples if len(samples.shape) <=1 else samples[:, channel]
+    s = stft(samples, binsize) # 431 * 513
+
+    #sshow : 431 * 256,
+    sshow, freq = logscale_spec(s, factor=1, sr=samplerate, alpha=alpha)
+    sshow = sshow[2:, :]
+    ims = 20.*np.log10(np.abs(sshow)/10e-6) # amplitude to decibel
+    timebins, freqbins = np.shape(ims)
+
+    ims = np.transpose(ims)
+    # ims = ims[0:256, offset:offset+768] # 0-11khz, ~9s interval
+    ims = ims[0:256, :] # 0-11khz, ~10s interval
+    #print "ims.shape", ims.shape
+
+    image = Image.fromarray(ims)
+    image = image.convert('L')
+    image.save(name)
+
+def create_spec(input_dir,save_img_dir,audio_type):
+
+        # file = open(input_dir, 'r')
+        for iter, line in enumerate(glob.glob(os.path.join(input_dir,"*.%s"%audio_type))):#enumerate(file.readlines()[1:]): # first line of traininData.csv is header (only for trainingData.csv)
+            # filepath = line.split(',')[0]
+            filename = line.split("/")[-1].split(".")[0]
+            # file_split.pop(-1)
+            # filename = "_".join(file_split)
+            if audio_type == "mp3":
+                wavfile = os.path.join(input_dir,filename + '.wav')
+                os.system('ffmpeg -i ' + line +" -ar 8000 tmp.wav" )
+                os.system("ffmpeg -i tmp.wav -ar 8000 tmp.mp3" )
+                os.system("ffmpeg -i tmp.mp3 -ar 8000 -ac 1 "+wavfile )
+                # os.system('ffmpeg -i ' + line +" " + wavfile)
+
+                # we create only one spectrogram for each speach sample
+                # we don't do vocal tract length perturbation (alpha=1.0)
+                # also we don't crop 9s part from the speech
+                plotstft(wavfile, channel=0, name=os.path.join(save_img_dir,filename+'.png'), alpha=1.0)
+                os.remove(wavfile)
+                os.remove("tmp.mp3")
+                os.remove("tmp.wav")
+            elif audio_type == "wav":
+                plotstft(line, channel=0, name=os.path.join(save_img_dir, filename + '_1.png'), alpha=1.0)
+            print "processed %d files" % (iter + 1)
+
+
+if __name__ == '__main__':
+    create_spec(input_dir = opt.input_dir,save_img_dir = opt.save_img_dir,audio_type = opt.audio_type)
---
+++ main_topcoder.py
@@ -0,0 +1,291 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 8/10/18 4:48 PM
+# @Author  : renxiaoming@julive.com
+# @Site    : 
+# @File    : main.py.py
+# @Software: PyCharm
+
+import torch
+import yaml
+import glob
+import time
+import PIL.Image as Image
+import torch.utils.data as data
+import Levenshtein
+#from torchviz import make_dot
+import os
+if torch.cuda.is_available():
+    import torch.cuda as device
+else:
+    import torch as device
+
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.optim.lr_scheduler import MultiStepLR
+import numpy as np
+import argparse
+from logger import Logger
+
+parser = argparse.ArgumentParser()
+#parser.add_argument('--root_dir', type=str,default='/Users/comjia/Downloads/code/pytorch_seq2seq/pytorch_seq2seq',help='??aishell????????path')
+parser.add_argument("--mode", type=str, default='train',help="train | dev | test")
+parser.add_argument("--audio_path", type=str, default="/movie/audio/topcoder/topcoder_test/fnn0slj2jrf.mp3",help="prepredicted audio path")
+parser.add_argument("--use_gpu", type=int, default=0,help="use gpu = 1; ")
+parser.add_argument("--use_pretrained", type=int, default=0,help="use_pretrained = 1; ")
+parser.add_argument("--model_path", type=str, default="checkpoint/spoken_Lang_id_topcoder",help="model path ")
+
+opt = parser.parse_args()
+print opt
+
+class batch_gen_imgdata(data.Dataset):
+    def __init__(self,data_listfile):
+        # self.root = os.path.expanduser(root)
+        # self.processed_imglist = glob.glob(os.path.join(data_path,"*.png"))
+        with open(data_listfile, "r") as f:
+            self.labels = f.readlines()
+        self.num_samples = len(self.labels)
+
+    def __getitem__(self, index):
+        img_path_label = self.labels[index]
+        #print img_path_label
+        img_split = img_path_label.split(",")
+        label = int(img_split[1][:-1])#kind_map[img_path.split("/")[-1].split("_")[0]]
+        img = Image.open(img_split[0])
+        img = np.array(img)
+        return img if img.shape[1] == 154 else img[:,:-1],label,img_split[0]
+
+    def __len__(self):
+        return self.num_samples
+
+    def collate_fn(self,batch):
+        # batch.sort(key=lambda x: len(x[1]), reverse=True)
+        imgs, labels, img_paths = zip(*batch)
+
+        #64 * 256 * 858
+        batch_imgs = np.array(imgs)
+        # 64 * 1
+        batch_labels = np.array(labels)[:,np.newaxis]
+        return batch_imgs,batch_labels
+
+class Network_CNN_RNN(nn.Module):
+    def __init__(self,rnn_input_size,rnn_hidden_size,rnn_num_layers,use_gpu =False):
+        super(Network_CNN_RNN, self).__init__()
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,      # input height
+                out_channels=16,    # n_filters
+                kernel_size=7,      # filter size
+                stride=1,           # filter movement/step
+                padding=0,      # , padding=(kernel_size-1)/2 ? stride=1
+            ),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),    # activation
+            nn.MaxPool2d(kernel_size=(3,3),stride=(2,2),padding=1),    # 16 * 125 *212
+        )
+        self.conv2 = nn.Sequential(  # input shape (16, 125, 212)
+            nn.Conv2d(16, 32, 5, 1, 0),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),  # activation
+            nn.MaxPool2d(3,2,1),  # output shape (32, 61 ,104)
+        )
+        self.conv3 = nn.Sequential(  # input shape (32, 61 ,104)
+            nn.Conv2d(32, 32, 3, 1, 0),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),  # activation
+            nn.MaxPool2d(3,2,1),  # output shape (32,30,51)
+        )
+        self.conv4 = nn.Sequential(  # input shape (32,30,51)
+            nn.Conv2d(32, 32, 3, 1, 0),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),  # activation
+            nn.MaxPool2d(3,2),  # output shape (32, 13, 24)
+        )
+        self.gru = nn.GRU(input_size=rnn_input_size, hidden_size=rnn_hidden_size,
+                          num_layers = rnn_num_layers,batch_first=True)
+        #self.batch_norm = nn.BatchNorm2d()
+        self.out = nn.Linear(rnn_hidden_size, lang_num)
+
+
+        self.use_gpu = use_gpu
+        if self.use_gpu:
+            self = self.cuda()
+
+    def forward(self, x):
+        #x: 100 * 1 * 256 * 858
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        # input: batchsize * 32 * 13 *51
+        x = x.view(x.size(0),-1,x.size(-1))
+        x = x.transpose(1,2)
+        gru_output,_ = self.gru(x)
+        gru_output = F.dropout(gru_output, training=self.training)
+        # gru_output = self.batch_norm(gru_output)
+        output = self.out(gru_output[:,-1,:])
+        #output = F.log_softmax(output, dim=1)
+        return output
+
+def train(network):
+    best_model_acc = 0.0
+    optimizer = torch.optim.Adam(net.parameters(), lr=0.01,weight_decay=1e-5)
+    scheduler_ = MultiStepLR(optimizer, milestones=[5,10,100], gamma=0.1)
+    objective = nn.CrossEntropyLoss()
+
+    for epoch in range(num_epochs):
+        network.train()
+        tr_loss = 0.0
+        tr_acc = 0.0
+        scheduler_.step()
+        for batch_index, (batch_imgs,batch_labels) in enumerate(train_set):
+            batch_imgs = Variable(torch.FloatTensor(batch_imgs))
+            batch_labels = Variable(torch.LongTensor(batch_labels))
+
+            if use_gpu:
+                batch_imgs = batch_imgs.cuda()
+                batch_labels = batch_labels.cuda()
+
+
+            pred = network(batch_imgs.unsqueeze(1))
+
+            loss = objective(input = pred,target = batch_labels.squeeze(1))
+
+            optimizer.zero_grad()
+            loss.backward() # backpropagation, compute gradients
+            optimizer.step() # apply gradients
+            batch_loss = loss.cpu().data.numpy() #/ batch_labels.shape[0]
+
+            _, keys = torch.topk(pred, 1)
+            pre = keys.cpu().data.numpy().T.tolist()[0]
+            tar = np.array(batch_labels).reshape([-1])
+            batch_acc = np.mean(np.equal(pre, tar))
+
+            tr_loss += batch_loss
+            tr_acc += batch_acc
+
+            # ========================= Log ======================
+            step = epoch * imgdata_train.num_samples + batch_index
+            # (1) Log the scalar values
+            info = {'loss': batch_loss, 'accuracy': batch_acc}
+
+            for tag, value in info.items():
+                logger_train.scalar_summary(tag, value, step)
+
+
+            if (batch_index+1) % verbose_step == 0:
+                print(
+                training_msg.format(time.asctime(), epoch + 1, batch_index + 1,
+                                    tr_loss / verbose_step, tr_acc / verbose_step))
+                tr_loss = 0.0
+                tr_acc = 0.0
+
+        if epoch % per_epoch_save_model == 0:
+            dev_acc = dev(network, epoch=epoch)
+            if dev_acc > best_model_acc:
+                torch.save(net.state_dict(), model_path)
+
+def dev(network,epoch = 0):
+    network.eval()
+    sum_acc = 0.0
+    for batch_index, (batch_imgs,batch_labels) in enumerate(test_set):
+        batch_imgs = Variable(torch.FloatTensor(batch_imgs))
+        batch_labels = Variable(torch.LongTensor(batch_labels))
+
+        if use_gpu:
+            batch_imgs = batch_imgs.cuda()
+            batch_labels = batch_labels.cuda()
+
+        pred = network(batch_imgs.unsqueeze(1))
+        _, keys = torch.topk(pred, 1)
+        pre = keys.cpu().data.numpy().T.tolist()[0]
+        tar = np.array(batch_labels).reshape([-1])
+        batch_acc = np.mean(np.equal(pre, tar))
+        sum_acc += batch_acc * len(batch_labels)
+        print("step_{:3d}_acc_{:.4f}".format(batch_index + 1,batch_acc))
+
+        # ========================= Log ======================
+        step = epoch * imgdata_test.num_samples + batch_index
+        # (1) Log the scalar values
+        info = { 'accuracy': batch_acc}
+
+        for tag, value in info.items():
+            logger_dev.scalar_summary(tag, value, step)
+    sum_acc = sum_acc/imgdata_test.num_samples
+    print "sum_acc = %.4f"%(sum_acc)
+    return sum_acc
+
+def test():
+    None
+
+def predict(network,img_path):
+    network.eval()
+
+    batch_imgs = Image.open(img_path)
+    batch_imgs = Variable(torch.FloatTensor(batch_imgs))
+    if use_gpu:
+        batch_imgs = batch_imgs.cuda()
+
+    pred = network(batch_imgs.unsqueeze(1))
+    _, keys = torch.topk(pred, 1)
+    pre = keys.cpu().data.numpy().T.tolist()[0]
+    print pre
+
+
+if __name__ == '__main__':
+
+    use_gpu = True if 1 == opt.use_gpu else False
+    use_pretrained = True if opt.use_pretrained == 1 else False
+    model_path = opt.model_path
+    lang_num = 179
+    rnn_hidden_size = 500
+    rnn_num_layers = 1
+    rnn_input_size = 416#448
+    num_epochs = 100
+    verbose_step = 10
+    per_epoch_save_model = 1
+
+    logger_train = Logger('./log_topcoder/logs_train')
+    logger_dev = Logger('./log_topcoder/logs_dev')
+    net = Network_CNN_RNN(rnn_input_size = rnn_input_size,rnn_hidden_size = rnn_hidden_size,rnn_num_layers = rnn_num_layers,use_gpu=use_gpu)
+    # net = Network_RNN(rnn_input_size = 256,rnn_hidden_size = rnn_hidden_size,rnn_num_layers = rnn_num_layers,use_gpu=use_gpu)
+    print net
+    params = list(net.parameters())
+    k = 0
+    for i in params:
+        l = 1
+        for j in i.size():
+            l *= j
+        k = k + l
+    print("all params num:" + str(k))
+
+
+    if use_pretrained:
+        net.load_state_dict(torch.load(model_path))
+
+
+    training_msg = 'time_{}_epoch_{:2d}_step_{:3d}_TrLoss_{:.4f}_acc_{:.4f}'
+    imgdata_train = batch_gen_imgdata(data_listfile="/movie/audio/topcoder/trainEqual.csv")
+    train_set = torch.utils.data.DataLoader(imgdata_train,
+                                              batch_size=64,
+                                              shuffle=True,
+                                              num_workers=1,
+                                              collate_fn=imgdata_train.collate_fn)
+
+    imgdata_test = batch_gen_imgdata(data_listfile="/movie/audio/topcoder/valEqaul.csv")
+    test_set = torch.utils.data.DataLoader(imgdata_test,
+                                              batch_size=64,
+                                              shuffle=False,
+                                              num_workers=1,
+                                              collate_fn=imgdata_test.collate_fn)
+
+    if opt.mode == "train":
+        train(net)
+    elif opt.mode == "dev":
+        dev(net)
+    elif opt.mode == "test":
+        test()
+    elif opt.mode == "predict":
+        predict(net,opt.img_path)
